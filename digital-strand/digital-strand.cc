b00d561a6cfc269bac0ffb7c20f7bf40ae991307
Ryan Anderson <ryan@michonline.com>
2015-10-25 23:08:51 -0700

    Better stepping

--- digital-strand/digital-strand.cc
+++ digital-strand/digital-strand.cc
@@ -45,7 +45,7 @@ byte MoveToTarget(byte current, byte target, byte step) {
 }
 
 byte SetStep(byte target, byte last) {
-  byte step = (target - last) / 16;
+  byte step = (target - last) / 8;
   if (step == 0) {
     if (target > last) {
       step = 1;
