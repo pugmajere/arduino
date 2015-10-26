47702dbe8373759e82889fec7350c9cf9e5aad51
Ryan Anderson <ryan@michonline.com>
2015-10-25 23:07:02 -0700

    Better colors?

--- digital-strand/digital-strand.cc
+++ digital-strand/digital-strand.cc
@@ -60,9 +60,9 @@ byte SetStep(byte target, byte last) {
 enum kColor_Names {RED, GREEN, BLUE};
 uint32_t color_index = 0;
 uint32_t colors[][3] = {{64,  0,  0},
-                        {64,  0, 64},
-                        {64, 64, 64},
-                        {0,   0, 64},
+                        {64, 32,  0},
+                        {64, 64,  0},
+                        {0,  64,  0},
                         {0,   0, 64},
                         {19,  0, 32},
                         {36,  0, 64},
