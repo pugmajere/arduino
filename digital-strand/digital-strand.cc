430fd2e7306293143a4113a29ebb3facf1255f93
Ryan Anderson <ryan@michonline.com>
2015-10-25 23:00:20 -0700

    New colors.

--- digital-strand/digital-strand.cc
+++ digital-strand/digital-strand.cc
@@ -60,7 +60,7 @@ byte SetStep(byte target, byte last) {
 enum kColor_Names {RED, GREEN, BLUE};
 uint32_t color_index = 0;
 uint32_t colors[][3] = {{64, 0, 0},
-                        {64, 32, 0},
+                        {64, 0, 64},
                         {64, 64, 0},
                         {0,  64, 0},
                         {0,   0, 64},
