4ae1d31fc675c82afb8d27151227a2bec489abe6
Ryan Anderson <ryan@michonline.com>
2016-09-09 22:05:26 -0700

    Make a purply runway thingy for Leslie.

--- strand-ws2811-runway/digital-strand.cc
+++ strand-ws2811-runway/digital-strand.cc
@@ -35,108 +35,37 @@ void setup() {
   strip1.show(); // Initialize all pixels to 'off'
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip1.numPixels(); i++) {
-    strip1.setPixelColor(i, c);
-    strip1.show();
-    delay(wait);
-  }
-}
-
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip1.numPixels(); i++) {
-      strip1.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip1.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip1.numPixels(); i++) {
-      strip1.setPixelColor(i, Wheel(((i * 256 / strip1.numPixels()) + j) & 255));
-    }
-    strip1.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip1.numPixels(); i=i+3) {
-        strip1.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip1.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip1.numPixels(); i=i+3) {
-        strip1.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip1.numPixels(); i=i+3) {
-        strip1.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip1.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip1.numPixels(); i=i+3) {
-        strip1.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
+#define DELAY_MS 75
+#define LED_DIV 15
 
 void SetColors(uint16_t pos, uint8_t r, uint8_t g, uint8_t b) {
-  strip1.setPixelColor(pos, r, g, b);
+  for (int i = 0; i < LED_DIV; i++) {
+    strip1.setPixelColor(pos + (NLEDS / LED_DIV * i), r, g, b);
+  }
 }
 
-#define DELAY_MS 1
-uint8_t leslie_colors[][3] = {{255, 0, 0}, {0, 255, 0}};
+uint8_t leslie_colors[][3] = {
+  {0, 0, 255}, // Blue
+  {96, 0, 128},
+  {0, 127, 127}, // cyan
+  {64, 0, 128},
+  {48, 0, 96},
+};
+
 uint8_t color_index = 0;
-uint16_t last_led = 0;
+uint16_t last_led = NLEDS / LED_DIV;
 unsigned long last_millis = 0;
 
 void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - last_millis > DELAY_MS) {
     SetColors(last_led, 0, 0, 0);
-    last_led = (last_led + 1) % NLEDS;
+
+    last_led--;
+    if (last_led > NLEDS) {
+      last_led = NLEDS / LED_DIV - 1;
+    }
+
     SetColors(last_led,
               leslie_colors[color_index][0],
               leslie_colors[color_index][1],
